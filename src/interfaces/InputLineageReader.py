724ed73b4c39b0d693858bd5cb15298216309099
kipariss1 <kpassudin@gmail.com>
2024-11-09 23:03:15 +0100

    finished advanced parcing

--- src/interfaces/InputLineageReader.py
+++ src/interfaces/InputLineageReader.py
@@ -7,13 +7,9 @@ class InputLineageReader(ABC):
 
     def __init__(self, input: Path):
         self._input_file = input
-        self.structure = defaultdict(self._tree)
         if not self._input_file.is_file():
             raise FileNotFoundError('input xml file is not found')
         self._read_input(self._input_file)
-
-    def _tree(self):
-        return defaultdict(self._tree)
 
     @abstractmethod
     def _read_input(self, target_file_path: Path):
