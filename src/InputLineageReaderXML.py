7e70628a7c56936ddbdd7eb00d4e6b482a3aff21
kipariss1 <kpassudin@gmail.com>
2024-11-11 23:04:18 +0100

    before redo

--- src/InputLineageReaderXML.py
+++ src/InputLineageReaderXML.py
@@ -52,10 +52,32 @@ class InputLineageReaderXML(InputLineageReader):
                 new_cls[attr + 'S'] = self._read_input_file_recursively(e, value[idx], new_cls)
         return ret_list
 
+    def _read_input_file_recursively_without_structure(self, root, parent):
+        ret_list = []
+        for i, ch in enumerate(root, start=1):
+            key = ch.tag
+            value = list(set([el.tag for el in ch]))
+            class_name = key[0] + key[1:].lower() + 'Class'
+            attributes_list = value
+            attributes = {el + 'S': [] for el in value}
+            lxml_attributes = {key.lower(): val for key, val in ch.attrib.items()}
+            attributes = {**attributes, **lxml_attributes}
+            cls = self._factory.derive_new_class(
+                class_name=class_name,
+                attributes=attributes,
+            )
+            new_cls = cls(ch, parent, i)
+            ret_list.append(new_cls)
+            for idx, attr in enumerate(attributes_list):
+                new_cls[attr + 'S'] = self._read_input_file_recursively_without_structure(ch, new_cls)
+        return ret_list
+
     def _read_input(self, target_file_path: Path):
         tree = etree.parse(target_file_path)
         root = tree.getroot()
-        self.__root = self._read_input_file_recursively(root, self._structure, None)
+        # self.__root = self._read_input_file_recursively(root, self._structure, None)
+        self.__root = self._read_input_file_recursively_without_structure(root, None)
+        pass
 
     @property
     def root(self):
